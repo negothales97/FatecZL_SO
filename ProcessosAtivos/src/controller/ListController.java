f3b79189dcebfed065b51e8630c908505d895d2f
negothales97 <thales.serra97@gmail.com>
2018-08-27 23:14:57 -0300

    Matar processo via PID ou nome, para linux e windows

---
+++ ProcessosAtivos/src/controller/ListController.java
@@ -0,0 +1,122 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.swing.JOptionPane;
+
+//Classe "ListController"
+public class ListController {
+//	Construtor "ListController"q
+	public ListController(){
+//		Classe para usar a classe "object" do java		
+		super();
+	}//Fim construtor "ListController"
+	
+	
+	
+//	Classe "processos"
+	public void processos(){
+		String so = System.getProperty("os.name");
+		
+			try {
+				Process proc = null;
+//				Realiza a leitura de um processo
+				if(so.contains("Windows 10")){
+					proc = Runtime.getRuntime().exec("TASKLIST /FO TABLE");
+				}
+				else{
+					if(so.contains("Linux")){
+						proc = Runtime.getRuntime().exec("ps -ef");
+					}
+				}
+				
+				
+//				Gera a busca pelas informações
+				InputStream fluxo = proc.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				
+//				Enquanto existir a lista, apresentar no console
+				while (linha != null){
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+				
+			} 
+//			Caso apresente erro, buscá-lo e apresentá-lo no console			
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+
+//	Classe "chamaProcesso" - Inicia processos dentro do computador
+	public void chamaProcesso(String path){
+//		Tenta rodar o "path"
+		try {
+			Runtime.getRuntime().exec(path);
+		} 
+//		Caso apresente o erro, será gerado uma string com o erro e buscará pelo código do erro
+		catch (IOException e) {
+			String erro = e.getMessage();
+		}
+	}//Fim classe "chamaProcesso"
+	
+	public void mataPid(String kill){
+//		Comando para finalizar o processo, junto com o PID
+		String so = System.getProperty("os.name");
+		String cmdPid = null;
+		if(so.contains("Windows 10")){
+			cmdPid = "TASKKILL /PID ";
+		}else{
+			if(so.contains("Linux")){
+				cmdPid = "kill -9 ";
+			}
+		}
+		int pid = 0;
+		
+		StringBuffer buffer = new StringBuffer();
+		
+//		Junta as variáveis dentro do sistema, para executar o comando
+		try {
+			pid = Integer.parseInt(kill);
+			buffer.append(cmdPid);
+			buffer.append(pid);
+		}
+//		Caso não rode o comando acima, é concatenado o dado presente na variável "cmdNome", com a "infoProcesso"
+		catch (NumberFormatException e){
+			e.printStackTrace();
+		}	
+		chamaProcesso(buffer.toString());
+	}
+	
+	public void mataNome(String kill){
+//		Comando para finalizar o processo, junto com o PID
+		String so = System.clearProperty("os.name");
+		
+		String cmdNome = null;
+		if(so.contains("Windows 10")){
+			cmdNome = "TASKKILL /IM ";
+		}else{
+			if(so.contains("Linux")){
+				cmdNome = "killall"; 
+			}
+		}
+		StringBuffer buffer = new StringBuffer();
+		
+//		Junta as variáveis dentro do sistema, para executar o comando
+		try {
+			buffer.append(cmdNome);
+			buffer.append(kill);
+		}
+//		Caso não rode o comando acima, é concatenado o dado presente na variável "cmdNome", com a "infoProcesso"
+		catch (NumberFormatException e){
+			e.printStackTrace();
+		}
+		chamaProcesso(buffer.toString());
+	}
+
+}//Fim classe "ListController"
---
+++ ProcessosAtivos/src/view/Main.java
@@ -0,0 +1,44 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.ListController;
+
+public class Main {
+	public static void main(String[] args) {
+		ListController list = new ListController();
+		
+		int opc = 0;
+		String kill = null;
+		
+		
+//		Meun de interação dos métodos
+		while(opc != 9){
+			opc = Integer.parseInt(JOptionPane.showInputDialog("[--SELECIONE UMA DAS OPCOES--] " +
+					"\n1 - Lista Processos do sistema\n2 - Finaliza processo via PID \n3 - Finaliza processo via nome"
+                        + "\n9 - Finaliza"));
+			switch (opc) {
+			case 1:
+				list.processos();
+				break;
+			case 2:
+				kill = JOptionPane.showInputDialog("Informe o programa que deseja finalizar via Pid:");
+				list.mataPid(kill);
+				break;
+			case 3:
+				kill = JOptionPane.showInputDialog("Informe o programa que deseja finalizar via nome:");
+				list.mataNome(kill);
+				break;
+			case 9:
+				JOptionPane.showMessageDialog(null, "FINALIZADO");
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "OPÇÃO INVÁLIDA!");
+				break;
+			}
+		}
+		
+
+		
+	}
+}
